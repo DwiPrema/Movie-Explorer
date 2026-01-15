b244069c817ee4108e9e86eea37d7045f76c1283
DwiPrema <madedwiprema08@gmail.com>
2026-01-15 20:47:48 +0800

    add the text widget

--- lib/widgets/widget_text.dart
+++ lib/widgets/widget_text.dart
@@ -1,1 +1,29 @@
 import 'package:flutter/material.dart';
+
+Widget title(String text, {TextAlign align = TextAlign.center, Color color = Colors.black, double fontSize = 20, FontWeight fontWeight = FontWeight.bold}) {
+  return Align(
+    child: Text(
+      text,
+      textAlign: align,
+      style: TextStyle(
+        color: color,
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+      ),
+    ),
+  );
+}
+
+Widget subtitle(String text, {TextAlign align = TextAlign.center, Color color = Colors.grey, double fontSize = 16, FontWeight fontWeight = FontWeight.w400}) {
+  return Align(
+    child: Text(
+      text,
+      textAlign: align,
+      style: TextStyle(
+        color: color,
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+      ),
+    ),
+  );
+}
