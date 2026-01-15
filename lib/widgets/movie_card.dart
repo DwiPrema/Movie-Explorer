ccdde495a936bfc0922a88e9efcbfaa56e2fd2d7
DwiPrema <madedwiprema08@gmail.com>
2026-01-15 20:50:46 +0800

    add card widget and setting dependencies

--- lib/widgets/movie_card.dart
+++ lib/widgets/movie_card.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_omdbid_api/models/movie_model.dart';
+import 'package:movie_omdbid_api/widgets/widget_text.dart';
 
 class MovieCard extends StatelessWidget {
   final MovieModel movie;
@@ -18,6 +19,8 @@ class MovieCard extends StatelessWidget {
       child: Column(
         children: [
           CachedNetworkImage(imageUrl: movie.poster),
+          title(movie.title),
+          subtitle(movie.year),
         ],
       ),
       
