37462239711aa9f47eaa1c1724f49ad78b749024
DwiPrema <madedwiprema08@gmail.com>
2026-01-16 19:32:32 +0800

    add constant color for the app

---
+++ lib/core/constant/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //primary color red dark
+  static const primaryColor = Color(0xff9E0008);
+
+  //neutral color, can use it for every widget
+  static const black = Color(0xff000000);
+  static const white = Color(0xffffffff);
+
+  //background color for scaffold / container
+  static const bgColor = Color(0xff212121);
+
+  //grey color for subtitle or less important text
+  static const grey = Color(0xffBABABA);
+}
