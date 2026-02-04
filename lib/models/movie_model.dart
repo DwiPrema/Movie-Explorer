efb449be540d707a0a2ec04e2e905b0f818c8184
DwiPrema <madedwiprema08@gmail.com>
2026-02-04 20:44:58 +0800

    delete unnecessary data

--- lib/models/movie_model.dart
+++ lib/models/movie_model.dart
@@ -3,14 +3,12 @@ class MovieModel {
   final String title;
   final String year;
   final String poster;
-  final DateTime? released;
 
   MovieModel({
     required this.imdbID,
     required this.title,
     required this.year,
     required this.poster,
-    this.released,
   });
 
   factory MovieModel.createObject(Map<String, dynamic> movie) {
@@ -21,16 +19,6 @@ class MovieModel {
       poster: movie["Poster"] != null && movie["Poster"] != "N/A"
           ? movie["Poster"]
           : "",
-    );
-  }
-
-  MovieModel copyWith({DateTime? released}) {
-    return MovieModel(
-      imdbID: imdbID,
-      title: title,
-      year: year,
-      poster: poster,
-      released: released ?? this.released,
     );
   }
 }
