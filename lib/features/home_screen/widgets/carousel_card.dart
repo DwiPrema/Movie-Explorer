ab3096dd66ebce1086e5c81822e3a0345238abd9
DwiPrema <madedwiprema08@gmail.com>
2026-02-21 16:35:16 +0800

    create and add app routes

--- lib/features/home_screen/widgets/carousel_card.dart
+++ lib/features/home_screen/widgets/carousel_card.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_omdbid_api/core/routes/routes.dart';
 import 'package:movie_omdbid_api/core/utils/responsive_helper.dart';
 import 'package:movie_omdbid_api/features/home_screen/bloc/movie_bloc.dart';
 import 'package:movie_omdbid_api/features/home_screen/data/models/movie_view_model.dart';
@@ -15,7 +16,12 @@ class CarouselCard extends StatelessWidget {
   final MovieCategory category;
   final bool isDates;
 
-  const CarouselCard({super.key, this.isDates = false, required this.textTitleCategory, required this.category});
+  const CarouselCard({
+    super.key,
+    this.isDates = false,
+    required this.textTitleCategory,
+    required this.category,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -31,56 +37,61 @@ class CarouselCard extends StatelessWidget {
           case MovieStatus.loading:
             return const Center(child: CircularProgressIndicator());
           case MovieStatus.success:
-            return Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: title(
-                          textTitleCategory,
-                          fontSize: 16,
-                          align: TextAlign.left,
+            return GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, AppRoutes.toDetail);
+              },
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: title(
+                            textTitleCategory,
+                            fontSize: 16,
+                            align: TextAlign.left,
+                          ),
                         ),
-                      ),
-                      isDates == true
-                          ? Expanded(
-                              child: subtitle(
-                                "Date Range : ${state.dates?.minimum} - ${state.dates?.maximum}",
-                                fontSize: 12,
-                                fontWeight: FontWeight.w700,
-                                align: TextAlign.right,
-                              ),
-                            )
-                          : const SizedBox.shrink(),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 16),
-                SizedBox(
-                  width: double.infinity,
-                  child: CarouselSlider(
-                    items: state.movies.where((m) => m.isValid).map((movie) {
-                      return MovieCard(movie: movie);
-                    }).toList(),
-                    options: CarouselOptions(
-                      height: ResponsiveHelper.heightCarousel(screenWidth),
-                      autoPlay: false,
-                      viewportFraction: ResponsiveHelper.viewportFraction(
-                        screenWidth,
-                      ),
-                      aspectRatio: 2 / 3,
-                      enableInfiniteScroll: false,
-                      initialPage: 0,
-                      enlargeCenterPage: false,
-                      padEnds: false,
+                        isDates == true
+                            ? Expanded(
+                                child: subtitle(
+                                  "Date Range : ${state.dates?.minimum} - ${state.dates?.maximum}",
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w700,
+                                  align: TextAlign.right,
+                                ),
+                              )
+                            : const SizedBox.shrink(),
+                      ],
                     ),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 16),
+                  SizedBox(
+                    width: double.infinity,
+                    child: CarouselSlider(
+                      items: state.movies.where((m) => m.isValid).map((movie) {
+                        return MovieCard(movie: movie);
+                      }).toList(),
+                      options: CarouselOptions(
+                        height: ResponsiveHelper.heightCarousel(screenWidth),
+                        autoPlay: false,
+                        viewportFraction: ResponsiveHelper.viewportFraction(
+                          screenWidth,
+                        ),
+                        aspectRatio: 2 / 3,
+                        enableInfiniteScroll: false,
+                        initialPage: 0,
+                        enlargeCenterPage: false,
+                        padEnds: false,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             );
           case MovieStatus.error:
             return Text(state.errorMessage ?? "Something went wrong :(");
